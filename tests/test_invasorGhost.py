fc5694fa81266e8c83038a6eed360425b1f75eff
Augusto Portillo <aportillo@desarrollosnea.com>
2019-03-27 20:48:05 -0300

    Invasor Ghost test_destruir_nave2

--- tests/test_invasorGhost.py
+++ tests/test_invasorGhost.py
@@ -16,3 +16,10 @@ class TestInvasorGhost(unittest.TestCase):
         nave = Nave(100, 60)
         invasor.destruir(nave)
         self.assertTrue(nave.Vida == 40)
+
+    def test_destruir_nave2(self):
+        invasor = InvasorGhost(100, 80)
+        nave = Nave(100, 60)
+        invasor.destruir(nave)
+        print(nave.Vida)
+        self.assertTrue(nave.Vida == 4)
